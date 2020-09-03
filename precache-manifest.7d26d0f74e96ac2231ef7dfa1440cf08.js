021b90a702a4bccc408a7773955fe95a9d99f7da
charcoallake <cjd3549@gmail.com>
2020-09-03 17:47:32 +0900

    Updates

---
+++ precache-manifest.7d26d0f74e96ac2231ef7dfa1440cf08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb947ec3c0763f91fe3779b2f317122e",
+    "url": "/yumry-tour/index.html"
+  },
+  {
+    "revision": "4090c1607fcd430be48c",
+    "url": "/yumry-tour/static/css/main.90566ae5.chunk.css"
+  },
+  {
+    "revision": "cd59acd7e1e08f7e36fe",
+    "url": "/yumry-tour/static/js/2.3b689d81.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yumry-tour/static/js/2.3b689d81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4090c1607fcd430be48c",
+    "url": "/yumry-tour/static/js/main.d0e45613.chunk.js"
+  },
+  {
+    "revision": "9563958b197838cabdd0",
+    "url": "/yumry-tour/static/js/runtime-main.8962fab5.js"
+  }
+]);
